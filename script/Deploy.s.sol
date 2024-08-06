90375806800cf949a82166829c9121893ec6fc98
echo <hujw77@gmail.com>
2024-08-06 11:38:30 +0800

    clean

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.13;
 
 import {Script, console2} from "forge-std/Script.sol";
-import {Counter} from "../src/Counter.sol";
 
 contract Deploy is Script {
     modifier broadcast() {
@@ -11,8 +10,5 @@ contract Deploy is Script {
         vm.stopBroadcast();
     }
 
-    function run() public broadcast {
-        Counter counter = new Counter();
-        counter.increment();
-    }
+    function run() public broadcast {}
 }
